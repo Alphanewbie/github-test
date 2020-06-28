e9012b7a6817c55361200aa5ba6031483f85af96
overmaind <overelectric@gmail.com>
2020-06-28 13:55:46 +0900

    update Setting

--- mine/settings.py
+++ mine/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import mine.SECRET_KEY_FILE
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ts52zkjo+nk=mw@8l32q%)atp=p&878q8ye+jyyw1sel!eax$u'
+SECRET_KEY = mine.SECRET_KEY_FILE.SECRET_KEY
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.azurewebsites.net/',
+    'localhost', '127.0.0.1', '[::1]',
+]
 
 
 # Application definition
